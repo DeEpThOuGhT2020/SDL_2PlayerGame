898043a77940ef4850ee18e76dade96a2a4b9bc7
DeEpThOuGhT2020 <suningh.2028@gmail.com>
2022-05-07 21:22:14 +0530

    Locations

---
+++ Locations.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string>
+#include "Player.h"
+#include "Constants.h"
+#include "Prof.h"
+#include <map>
+
+map < pair<int,int> , string> locations ;
+locations.insert(pair<pair<int,int>>, string>(<720,420>, "Collect CMP Notes from Scoops"));
+locations.insert(pair<pair<int,int>>, string>(<408,516>, "Buy ORS from Apollo"));
+locations.insert(pair<pair<int,int>>, string>(<252,420>, "Get your cycle repaired"));
+locations.insert(pair<pair<int,int>>, string>(<84,660>, "Get milk from mother diary"));
+locations.insert(pair<pair<int,int>>, string>(<540,552>, "Play in the Sports Ground"));
+locations.insert(pair<pair<int,int>>, string>(<396,648>, "Visit the Research Park"));
+locations.insert(pair<pair<int,int>>, string>(<348,312>, "Eat at Satpura Night Mess"));
